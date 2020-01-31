61f73e52d3b2a83d3d1ae2358e0a043e1e48ce72
Akinjide Bankole <r@akinjide.me>
2020-01-31 20:25:16 +0100

    feat: add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="innovation-sandbox",
+    version="1.0.3",
+    description="Python package for Innovation Sandbox",
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    url = "https://github.com/enyata/innovation-sandbox-python",
+    # license="LICENSE.txt",
+)
