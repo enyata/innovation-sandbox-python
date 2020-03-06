a22e785354c0dfc205fe0bec8070508ad89efdc5
Tunmisesuliat <tunmisesuliat@gmail.com>
2020-03-06 15:44:24 +0000

    updated travis file

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="innovation-sandbox",
     version="1.1.0",
     description="Python package for Innovation Sandbox",
-    long_description=open("README.rst").read(),
+    # long_description=open("README.rst").read(),
     packages=find_packages(),
     url = "https://github.com/enyata/innovation-sandbox-python",
     license='MIT',
