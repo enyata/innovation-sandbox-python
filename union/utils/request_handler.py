78d60146b18de61176bb2503c91d7866b3715f56
Israel Ugbodaga <israelugbodaga@gmail.com>
2020-03-24 07:44:39 +0100

    update modules
    Signed-off-by: Israel Ugbodaga <israelugbodaga@gmail.com>

--- union/utils/request_handler.py
+++ union/utils/request_handler.py
@@ -2,7 +2,6 @@ import requests
 from .response_handler import response
 import json
 
-
 def request(url, payload, headers):
     res = requests.post(url=url, data=payload, headers=headers).json()
     return response(res)
